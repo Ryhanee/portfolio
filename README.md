6876af816dbe2b2eaf7bfbe4760872073b41088d
Ryhanee <dalhoumrihane@gmail.com>
2025-03-28 14:14:49 +0100

    first commit of portfolio

---
+++ README.md
@@ -0,0 +1,24 @@
+> Welcome to my portfolio website! This site showcases my skills, experience, education, projects, and how to get in touch with me. It's built with React and Vite for a fast, modern web experience. You can explore my work, learn about my journey, and contact me directly.
+
+## Sections of the Portfolio
+
+The portfolio website consists of the following sections:
+
+---
+
+## 💻 Technologies Used  
+- **Frontend**: React.js with Vite (TypeScript)  
+- **Styling**: Tailwind CSS  
+- **UI Components**: shadcn/ui  
+- **Animations**: Framer Motion  
+- **Icons**: Lucide React  
+- **Deployment**: Vercel  
+
+**About**: Introduction and a brief overview.  
+**Skills**: A detailed list of my technical skills.  
+**Experience**: My professional journey and work experience.  
+**Education**: Academic background and certifications.  
+**Projects**: A showcase of the projects I've worked on.  
+**Contact**: Information on how to reach out to me.  
+
+```
---
+++ src/config/config.ts
@@ -0,0 +1,35 @@
+const config = {
+  information: {
+    name: "Rihane Dalhoum",
+    helloText: "Hello",
+    welcomeText: "✨ Welcome to my universe",
+    title: "Full stack developer 🚀 | Data scientist",
+    title3: "and coding the future 💻 ✨",
+  },
+  flipWords: ["Linux & GitHub for DevOps", "Cloud Computing", "Crafting frameworks", "Frontend Web Developer"],
+  codeContent: {
+    code: `const profile = {
+  name: 'Rihane Dalhoum',
+  title: 'Full-Stack Developer | Data scientist | Problem Solver',
+  skills: [
+    'React', 'NextJS', 'Redux', 'Express','Angular','Python'
+    'MySQL', 'MongoDB', 'Docker', 'AWS', 'TypeScript',
+    'GraphQL', 'Git', 'Linux', 'Discord Development'
+  ],
+  hardWorker: true,
+  quickLearner: true, 
+  problemSolver: true,
+  yearsOfExperience: 7,
+  hireable: function() {
+    return (
+      this.hardWorker &&
+      this.problemSolver &&
+      this.skills.length > 5 &&
+      this.yearsOfExperience >= 3
+    );
+  }
+};`,
+  },
+};
+
+export default config;
